7cb36b6b0614fd84c5136d36e79d3dfdbe7dddcf
Aleksei Ents <99408143+djo00771@users.noreply.github.com>
2022-05-07 22:47:53 +0300

    add calc

---
+++ Test API Calculation/Calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def multiply(self, x, y):
+        return x * y
+
+    def divizion(self, x, y):
+        return x / y
+
+    def substraction(self, x, y):
+        return x - y
+
+    def adding(self, x, y):
+        return x + y
